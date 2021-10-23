9d08434aade620fec7aaf23dd4a4f0a872391a18
Anubhav1603 <anubhavsinha110@gmail.com>
2021-10-23 15:44:05 +0530

    formatted file

--- Practice exercise/question15.c
+++ Practice exercise/question15.c
@@ -1,18 +1,17 @@
 /*
 Write a program to print first n natural numbers using for loop.
 */
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
     int n;
 
     printf("Please enter a number to print counting till that: ");
-    scanf("%d",&n);
+    scanf("%d", &n);
 
-    for(int i = 1; i <= n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        printf("%d\n",i);
+        printf("%d\n", i);
     }
-    
 }
